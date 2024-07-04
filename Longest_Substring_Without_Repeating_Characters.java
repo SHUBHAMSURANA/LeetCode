fe7fae0f9022dd5f53edf6db9704be977c6eb47e
SHUBHAM SURANA <shubhamsurana1300@gmail.com>
2024-07-05 02:34:22 +0530

    Create Longest_Substring_Without_Repeating_Characters.java

---
+++ Longest_Substring_Without_Repeating_Characters.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+
+        // Create 2 hash map
+        // one hash map for window freq
+        // one hash map for character position in array.
+        Map<Character,Integer> loc = new HashMap<>();
+        Map<Character,Integer> freq = new HashMap<>();
+
+        for(int right = 0;right<len;right++){
+
+            char curr = s.charAt(right);
+            freq.put(curr, freq.getOrDefault(curr, 0)+1);
+
+            if (freq.get(curr) > 1 && loc.get(curr) >= left) {
+                left = loc.get(curr)+1;
+                freq.put(curr, freq.get(curr)-1);
+            }
+
+            max = Math.max(max, right-left+1);
+            loc.put(curr, right);
+        }
+        return max;
+    }
+}
